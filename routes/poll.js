5562ca4fc463f4edcd66c0316db550c1c5dc02df
Jeremy Hurley <hurleyjer@yahoo.com>
2016-11-01 20:46:08 -0400

    working graph

--- routes/poll.js
+++ routes/poll.js
@@ -2,20 +2,37 @@ var express = require('express');
 var router = express.Router();
 var csrf = require('csurf');
 var passport = require('passport');
-var url = require('url');
+
+var Poll = require('../models/poll');
+
 
 var csrfProtection = csrf();
 router.use(csrfProtection);
 
 router.get('/', function(req, res, next){
   if(req.query.id){
-    console.log(req.query.id);
-    res.send(req.query.id);
+    Poll.findById(req.query.id, function(err, poll){
+      res.render('./poll/viewPoll', { poll: poll});
+    })
   }else{
   res.redirect('/');
 };
 });
 
+/*
+var express = require('express');
+var router = express.Router();
+
+var User = require('../../models/user');
+var Poll = require('../../models/poll');
+
+var currentPoll = Poll.findOne({_id: id}, function(err, doc){
+  if(err){
+    console.log(err);
+  }
+  console.log(doc + 'doc');
+});
+*/
 
 module.exports = router;
 
