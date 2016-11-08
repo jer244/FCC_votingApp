a60f094d56740a5a9efd42bb705a0e8375f74cdf
Jeremy Hurley <hurleyjer@yahoo.com>
2016-11-08 09:35:28 -0500

    working graphs

--- routes/poll.js
+++ routes/poll.js
@@ -6,46 +6,72 @@ var passport = require('passport');
 var Poll = require('../models/poll');
 
 
-var csrfProtection = csrf();
+var csrfProtection = csrf({cookie: true});
+
 router.use(csrfProtection);
 
-router.get('/', function(req, res, next){
-  if(req.query.id){
-    Poll.findById(req.query.id, function(err, poll){
-      res.render('./poll/viewPoll', { poll: poll});
-    })
-  }else{
-  res.redirect('/');
-};
+router.get('/vote', function(req, res, next) {
+    if (req.query.id && req.query.entry) {
+        Poll.findById(req.query.id, function(err, poll) {
+            for (var i = 0; i < poll.pollOptions.length; i++) {
+                if (poll.pollOptions[i].optionTitle == req.query.entry) {
+                    poll.pollOptions[i].votes += 1;
+                    poll.save(function(err, updatedPoll) {
+                        if (err) throw err;
+                        res.redirect('/poll?id=' + req.query.id);
+                    })
+                }
+            };
+        });
+    } else {
+        res.redirect('/');
+    };
 });
 
-/*
-var express = require('express');
-var router = express.Router();
-
-var User = require('../../models/user');
-var Poll = require('../../models/poll');
-
-var currentPoll = Poll.findOne({_id: id}, function(err, doc){
-  if(err){
-    console.log(err);
-  }
-  console.log(doc + 'doc');
+router.post('/vote', function(req, res, next){
+  if (req.body.writeIn) {
+      Poll.findById(req.body.id, function(err, poll) {
+          poll.pollOptions.push({
+            optionTitle: req.body.writeIn,
+            votes: 1
+          });
+          poll.save(function(err, updatedPoll) {
+              if (err) throw err;
+              res.redirect('/poll?id=' + req.body.id);
+          })
+        });
+          } else {
+      res.redirect('/');
+  };
 });
-*/
+
+
+router.get('/', function(req, res, next) {
+    if (req.query.id) {
+        Poll.findById(req.query.id, function(err, poll) {
+            res.render('./poll/viewPoll', {
+                poll: poll,
+                csrfToken: req.csrfToken()
+            });
+        })
+    } else {
+        res.redirect('/');
+    };
+});
+
 
 module.exports = router;
 
 function isLoggedIn(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/user/logIn');
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/user/logIn');
 }
 
 function notLoggedIn(req, res, next) {
-  if (!req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/');
+    if (!req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/');
 }
